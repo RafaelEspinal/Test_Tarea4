f139a42b605e8300e5473ad4c6dee2045e2be340
edwal <edwallg@gmail.com>
2020-02-24 02:27:55 -0400

    iniciando proyecto nuevo

---
+++ Tarea4/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tarea4.Models;
+
+namespace Tarea4.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Formulario()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Formulario(datos data, HttpPostedFileBase imagen)
+        {
+            if (ModelState.IsValid)
+            {
+                string nombreFoto = imagen.FileName;
+
+                imagen.SaveAs(Server.MapPath("/Content/" + nombreFoto));
+
+
+                @ViewBag.foto = nombreFoto;
+
+                return View("Resivir", data);
+            }
+            else
+            {
+
+                return View("Formulario");
+            }
+        }
+        public ActionResult Resivir(datos data)
+        {
+
+            return View(data);
+        }
+    }
+}
---
+++ Tarea4/Models/datos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tarea4.Models
+{
+    public class datos
+    {
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string Nombre { set; get; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string Apellido { set; get; }
+        [Range(15, 100)]
+        public int Edad { set; get; }
+        public string Teléfono { set; get; }
+
+        [Required(ErrorMessage = "Este campo es requerido.")]
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*",
+          ErrorMessage = "Dirección de Correo electrónico incorrecta.")]
+        public string Correo { set; get; }
+        public string Género { set; get; }
+        public puesto_trabajo trabajo { set; get; }
+
+
+       
+    }
+
+    public enum puesto_trabajo
+    {
+
+       puesto1 , puesto2 , puesto3
+    }
+
+}
